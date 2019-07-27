adb458b4f8a4cf9d340304d79e0f5055339c420e
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2019-07-27 18:23:58 +0530

    new concepts

--- 2.Featers/varibles.ts
+++ 2.Featers/varibles.ts
@@ -12,3 +12,28 @@ let colors: string[] = ["red", "blue", "green"];
 let myNumbers: number[] = [1, 2, 3, 45, 7];
 let truths: boolean[] = [true, false, false];
 
+// classes
+
+class Car {}
+
+let car: Car = new Car();
+
+// object literals
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
+
+//function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// hen to use anotation
+// fuctions that returns any type
+
+const json = '{"x":10,"y":20}';
+
+const coordinates = JSON.parse(json);
+console.log(coordinates)
