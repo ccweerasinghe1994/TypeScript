aa8fe42f20f8effdc567485bd7d97ae9a58798d6
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2019-07-27 15:52:44 +0530

    let is introduced

--- 2.Featers/varibles.ts
+++ 2.Featers/varibles.ts
@@ -1,3 +1,7 @@
-let apples:number = 34;
+let apples: number = 34;
+let speed: string = "fast";
+let hasName: boolean = true;
+let nothingMuch:null = null;
+let nothing:undefined = undefined;
 
-apples = 34;
+
