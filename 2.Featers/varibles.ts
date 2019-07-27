506bffbb3372b5c6a0783f10283492f11b97dcc8
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2019-07-27 17:01:35 +0530

    arrays

--- 2.Featers/varibles.ts
+++ 2.Featers/varibles.ts
@@ -1,7 +1,14 @@
 let apples: number = 34;
 let speed: string = "fast";
 let hasName: boolean = true;
-let nothingMuch:null = null;
-let nothing:undefined = undefined;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
 
+//! built in  objects
+
+let now: Date = new Date();
+
+let colors: string[] = ["red", "blue", "green"];
+let myNumbers: number[] = [1, 2, 3, 45, 7];
+let truths: boolean[] = [true, false, false];
 
