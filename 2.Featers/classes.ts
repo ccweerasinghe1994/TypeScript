5080990d11ba56a9c44471b755ec494c52a01fe4
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2019-07-28 13:34:47 +0530

    classes

---
+++ 2.Featers/classes.ts
@@ -0,0 +1,35 @@
+class Vehicle {
+    
+    constructor(public color:string){
+
+        
+
+    }
+  public drive(): void {
+    console.log("chugga chugga");
+  }
+
+  public honk(): void {
+    console.log("beep");
+  }
+// }
+class Car extends Vehicle {
+    constructor(public wheels:number,color:string){
+        super('red');
+
+    }
+   drive(): void {
+    console.log("vroom");
+  }
+}
+const vehicle = new Vehicle('orange');
+
+// console.log("hello w");
+// vehicle.honk();
+// vehicle.honk();
+// vehicle.honk();
+// vehicle.honk();
+// vehicle.drive();
+
+const car = new Car(12,'vlue');
+// car.drive();
