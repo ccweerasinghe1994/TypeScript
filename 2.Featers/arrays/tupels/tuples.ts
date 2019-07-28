fda76e17e9e8e9d5938dbab4bc9da06b2047fa13
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2019-07-28 07:48:11 +0530

    interfaces

---
+++ 2.Featers/arrays/tupels/tuples.ts
@@ -0,0 +1,17 @@
+const drink = {
+  color: "brown",
+  carbonated: true,
+  suger: 40
+};
+
+type drink = [string, boolean, number];
+
+const pepsi: drink = ["brown", true, 40];
+const sprite: drink = ["green", true, 50];
+
+const carSpecs: [number, number] = [2300, 230];
+
+const carspecs = {
+  hoesePower: 2300,
+  weight: 230
+};
