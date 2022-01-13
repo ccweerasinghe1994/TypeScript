ce6098e75ec4320ba8c18c582adb9a7a7f18baba
wchamara <chamara.weerasinghe@my.sliit.lk>
2022-01-13 09:57:28 +0530

    adding type anotation for objects

--- 2.Featers/annotaions/objects.ts
+++ 2.Featers/annotaions/objects.ts
@@ -3,15 +3,15 @@ const profile = {
   age: 20,
   coords: {
     lat: 0,
-    lng: 15
+    lng: 15,
   },
   setAge(age: number): void {
     this.age = age;
-  }
+  },
 };
+
 
 const { age,name }: { age: number;name:string } = profile;
 const {
   coords: { lat, lng }
 }: { coords: { lat: number; lng: number } } = profile;
- 
