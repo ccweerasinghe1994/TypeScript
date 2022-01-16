77c4beb48e2e7315cb6588152dcdd2ea1d5ccb45
wchamara <chamara.weerasinghe@my.sliit.lk>
2022-01-16 08:02:29 +0530

    ädding interface types

--- 2.Featers/classes.ts
+++ 2.Featers/classes.ts
@@ -1,35 +1,43 @@
-class Vehicle {
-    
-    constructor(public color:string){
+// class Vehicle {
 
-        
+//     constructor(public color:string){
 
-    }
-  public drive(): void {
-    console.log("chugga chugga");
-  }
+//     }
+//   public drive(): void {
+//     console.log("chugga chugga");
+//   }
 
-  public honk(): void {
-    console.log("beep");
-  }
+//   public honk(): void {
+//     console.log("beep");
+//   }
+// // }
+// class Car extends Vehicle {
+//     constructor(public wheels:number,color:string){
+//         super('red');
+
+//     }
+//    drive(): void {
+//     console.log("vroom");
+//   }
 // }
-class Car extends Vehicle {
-    constructor(public wheels:number,color:string){
-        super('red');
+// const vehicle = new Vehicle('orange');
 
-    }
-   drive(): void {
-    console.log("vroom");
+// // console.log("hello w");
+// // vehicle.honk();
+// // vehicle.honk();
+// // vehicle.honk();
+// // vehicle.honk();
+// // vehicle.drive();
+
+// const car = new Car(12,'vlue');
+// // car.drive();
+
+class Vehicle {
+  drive(): void {
+    console.log("chugga chhugga");
   }
 }
-const vehicle = new Vehicle('orange');
 
-// console.log("hello w");
-// vehicle.honk();
-// vehicle.honk();
-// vehicle.honk();
-// vehicle.honk();
-// vehicle.drive();
+const vehicle = new Vehicle();
 
-const car = new Car(12,'vlue');
-// car.drive();
+vehicle.drive();
--- 2.Featers/interfaces.ts
+++ 2.Featers/interfaces.ts
@@ -1,26 +1,28 @@
 interface Reportable {
   summery(): string;
 }
+
 const oldCivic = {
   name: "civic",
   year: new Date(),
   broken: true,
   summery(): string {
     return `Name:${this.name}`;
-  }
+  },
 };
 
 const drink = {
   color: "brown",
-  carbanated: true,
-  suger: 40,
+  carbonated: true,
+  sugar: 40,
   summery(): string {
-    return `My drink has ${this.suger} grams of suger`;
-  }
+    return `My drink has ${this.sugar} grams of sugar`;
+  },
 };
 
 const printSummery = (item: Reportable): void => {
-  console.log(item.summery());
+  const { summery } = item;
+  console.log(summery());
 };
 
 printSummery(oldCivic);
